c8ec22eec0e483730c07601ac3a899e4a64ae02e
Rafaelsacra <rafaelsacra@gmail.com>
2024-12-03 17:29:34 -0300

    introautogui

---
+++ automacao.py
@@ -0,0 +1,12 @@
+import pyautogui   
+import time
+#RPA - PEGAR POSIÇÕES DO MOUSE E DA TELA
+print(pyautogui.position())
+print(pyautogui.size())
+
+#RPA - FUNÇÕES DO MOUSE
+time.sleep(5)
+pyautogui.click(x=1605, y=105)
+
+
+ 
---
+++ pegar_posicao.py
@@ -0,0 +1,5 @@
+import pyautogui   
+import time
+#RPA - PEGAR POSIÇÕES DO MOUSE E DA TELA
+time.sleep(5)
+print(pyautogui.position())
