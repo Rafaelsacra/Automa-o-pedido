951471c09adc4e853f227c2c45ab811a667d3561
Rafaelsacra <rafaelsacra@gmail.com>
2025-01-09 14:36:15 -0300

    teste

--- automacao.py
+++ automacao.py
@@ -1,9 +1,15 @@
+import pandas as pd
 from basepandas import carregar_dados
 import pyautogui   
 import time
 
+# Lista de arquivos Excel
+arquivos = [
+    "frios2.xlsx", "frios4.xlsx", "frios5.xlsx", "frios7.xlsx", "frios8.xlsx",
+    "cond2.xlsx", "cond4.xlsx", "cond5.xlsx", "cond7.xlsx", "cond8.xlsx"
+]
 
-caminho_arquivo = r'C:/Users/Usuario/Desktop/Automaorder/teste.xlsx'
+caminho_arquivo = r'C:/Users/Usuario/Desktop/Automaorder/frios8.xlsx'
 
 dados = carregar_dados(caminho_arquivo)
 
@@ -15,18 +21,23 @@ for index, row in dados.iterrows():
     print(pyautogui.position())
     print(pyautogui.size())
 
-    time.sleep(3)
+    time.sleep(2)
 
     #RPA - FUNÇÕES DO MOUSE
     time.sleep(0.5)
     pyautogui.click(x=1612, y=104)
 
     #codigo do produto
-    pyautogui.write(str(row['codigo']))
+    codigo = row['codigo']
+    pyautogui.write(str(codigo))
     pyautogui.press('down')
 
+    if codigo == 0:
+        print("Código zero encontrado. Parando o PyAutoGUI.")
+        break
+
     # transferencia
-    pyautogui.click(x=1879, y=463)
+    pyautogui.click(x=1978, y=477)
 
     # quantidade de pedido
     pyautogui.write(str(row['quantidade']))
@@ -34,6 +45,8 @@ for index, row in dados.iterrows():
 
     #clica no produto
     pyautogui.click(x=1574, y=347)
+
+  
 
 
  
--- basepandas.py
+++ basepandas.py
@@ -2,9 +2,19 @@ from pathlib import Path
 import pandas as pd 
 
 
-def carregar_dados(caminho_arquivo):
+#def carregar_dados(caminho_arquivo):
+    # Função que lê e retorna os dados necessários
+    #df = pd.read_excel(caminho_arquivo)
+   # return df[['codigo', 'quantidade']]
+
+def carregar_dados(caminho_arquivo): 
     # Função que lê e retorna os dados necessários
     df = pd.read_excel(caminho_arquivo)
+    
+    # Tratar valores ausentes em 'codigo' e 'quantidade'
+    df['codigo'] = df['codigo'].fillna(0).astype(int)  # Preenche valores ausentes com 0 e converte para int
+    df['quantidade'] = df['quantidade'].fillna(0).astype(int)  # Preenche valores ausentes com 0 e converte para int
+    
     return df[['codigo', 'quantidade']]
 
 #colunas_selecionadas = df[['codigo', 'quantidade']]
